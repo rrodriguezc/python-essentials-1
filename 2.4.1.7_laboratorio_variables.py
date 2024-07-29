989efe66c29be9d581312eb74e7eb2f134bdda08
Rodrigo Rodríguez <ra.rodriguezco@gmail.com>
2024-07-29 17:43:19 -0400

    2.4.1.7 Laboratorio: Variables

---
+++ 2.4.1.7_laboratorio_variables.py
@@ -0,0 +1,22 @@
+"""
+Escenario
+A continuación una historia:
+
+Érase una vez en la Tierra de las Manzanas, Juan tenía tres manzanas, María tenía cinco manzanas, y Adán tenía seis manzanas. Todos eran muy felices y vivieron por muchísimo tiempo. Fin de la Historia.
+
+Tu tarea es:
+
+Crear las variables: juan, maria, y adan.
+Asignar valores a las variables. El valor debe de ser igual al número de manzanas que cada quien tenía.
+Una vez almacenados los números en las variables, imprimir las variables en una línea, y separar cada una de ellas con una coma.
+Después se debe crear una nueva variable llamada total_manzanas y se debe igualar a la suma de las tres variables anteriores.
+Imprime el valor almacenado en total_manzanas en la consola.
+"""
+
+juan=3
+maria=5
+adan=6
+
+print(juan,maria,adan,sep=",")
+total_manzanas=juan+maria+adan
+print(total_manzanas)
