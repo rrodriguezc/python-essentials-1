d73abcfedc0f0296bdb50689f69ecfba12a97c29
Rodrigo Rodríguez <ra.rodriguezco@gmail.com>
2024-07-30 13:10:54 -0400

    2.6.1.9 Laboratorio:Entradas y salidas simples

---
+++ 2.6.1.9_laboratorio_entradas_salidas_simples.py
@@ -0,0 +1,18 @@
+"""
+Escenario
+La tarea es completar el código para evaluar y mostrar el resultado de cuatro operaciones aritméticas básicas.
+
+El resultado debe ser mostrado en consola.
+
+Quizá no podrás proteger el código de un usuario que intente dividir entre cero. Por ahora, no hay que preocuparse por ello.
+"""
+
+a = float(input("Ingresa el primer valor: "))
+b = float(input("Ingresa el segundo valor: "))
+
+print("Suma:", a + b)
+print("Resta:", a - b)
+print("Multiplicación:", a * b)
+print("División:", a / b)
+
+print("\n¡Eso es todo, amigos!")
