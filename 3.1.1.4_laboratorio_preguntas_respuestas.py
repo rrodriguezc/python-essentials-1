10d3f75caafb484fa979c00081ef99a713e3c8d5
Rodrigo Rodríguez <ra.rodriguezco@gmail.com>
2024-08-01 11:15:17 -0400

    3.1.1.4 Laboratorio: Preguntas y respuestas

---
+++ 3.1.1.4_laboratorio_preguntas_respuestas.py
@@ -0,0 +1,10 @@
+"""
+Escenario
+Usando uno de los operadores de comparación en Python, escribe un programa
+simple de dos líneas que tome el parámetro n como entrada, que es un entero, e imprime False si n es menor que 100, y True si n es mayor o igual que 100.
+
+No debes crear ningún bloque if (hablaremos de ellos muy pronto). Prueba tu
+código usando los datos que te proporcionamos.
+"""
+n = int(input("Ingresa un número: "))
+print(n >= 100)
